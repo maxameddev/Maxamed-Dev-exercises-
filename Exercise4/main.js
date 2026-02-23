33d2cfe289926e2f32cf6843eb53a82f0c17afd4
maxameddev <engmaxamed40@gmail.com>
2026-02-23 09:33:09 +0300

    Submit Exercise 4

---
+++ Exercise4/main.js
@@ -0,0 +1,27 @@
+//  Function Declaration
+
+function addTwoNumbers(num1, num2) {
+
+    console.log("Wadarta guud waa: " + (num1 + num2));
+
+}
+
+addTwoNumbers(5, 10);
+addTwoNumbers(20, 30);
+addTwoNumbers(100, 200);
+addTwoNumbers(1, 2);
+
+// let fisrtName = "maxamed";
+// let middalename = "Xassan";
+// let lastName = "Cumar";
+// console.log("Magacaygu waa: " + fisrtName + " " + middalename + " " + lastName);
+
+
+// Function Expression
+let multipleTwoNumbers2 = function (num1, num2) {
+
+    console.log("Jawaabtu waa: " + (num1 * num2))};
+
+    multipleTwoNumbers2(5, 10);
+    multipleTwoNumbers2(20, 30);
+    multipleTwoNumbers2(100, 200);
