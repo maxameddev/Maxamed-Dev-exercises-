7adf23307684943cc3efd14fefaf65ed43c6e068
maxameddev <engmaxamed40@gmail.com>
2026-02-24 08:28:59 +0300

    submit js exercise6

---
+++ Exercise6/main.js
@@ -0,0 +1,9 @@
+// Array and object exercise
+
+let colors = ["red", "blue", "green", "yellow", "orange"];
+
+console.log(colors[0]); 
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
