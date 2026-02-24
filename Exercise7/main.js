cd9d544b8fa69e65128715ec4b7ab1a94394038a
maxameddev <engmaxamed40@gmail.com>
2026-02-24 16:56:17 +0300

    js exercise 7

---
+++ Exercise7/main.js
@@ -0,0 +1,20 @@
+let colors = ["red", "blue", "green", "yellow", "orange"];
+
+console.log(colors[0]); 
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
+
+colors.push("purple");
+
+console.log(colors[5]);
+
+colors.pop();
+
+console.log(colors[5]);
+
+ colors.unshift("pink");
+
+ console.log(colors[0]);
