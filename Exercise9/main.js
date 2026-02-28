461856c2098b44e757191252ee29af8c87dfe060
maxameddev <engmaxamed40@gmail.com>
2026-02-28 16:59:27 +0300

    js exercise 9

---
+++ Exercise9/main.js
@@ -0,0 +1,19 @@
+
+let car = {
+    make: "Honda",
+    model: "Civic",
+    year: 2021,
+    color: "Red",
+
+    run: function() {
+        console.log("The car is running.");
+    }
+};
+
+console.log(car);
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+console.log(car.color);
+
+car.run()
