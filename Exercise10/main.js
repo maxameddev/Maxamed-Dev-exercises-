8d0e97b7a657b13d68915c6a98ffd7a7e1ed9b9a
maxameddev <engmaxamed40@gmail.com>
2026-03-01 16:48:12 +0300

    Submit Exercise10

---
+++ Exercise10/main.js
@@ -0,0 +1,16 @@
+
+// Array of objects
+
+let library = [
+
+    {title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925,},
+    {title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960, }
+]
+
+console.log(library);
+console.log(library[0].title);
+console.log(library[1].author);
