d86332eb7493e78ed3c90b426e95935586c16f3c
maxameddev <engmaxamed40@gmail.com>
2026-03-02 16:17:21 +0300

    js exercise12

---
+++ exercise12/main.js
@@ -0,0 +1,6 @@
+// meaningfull for loop
+
+let Qudaarta =["muus", "canab", "tufaax", "moos", "baradhada", "basal", "karootada", "cabbage", "spinach", "kale"];
+
+for(let i=0; i<Qudaarta.length; i++)
+    {console.log(Qudaarta[i]);}
