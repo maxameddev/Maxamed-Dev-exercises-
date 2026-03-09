a8c662ccb2fedfd1e67e65eae0345a881fca2239
maxameddev <engmaxamed40@gmail.com>
2026-03-09 16:24:49 +0300

    js exercise16

---
+++ Exercise16/main.js
@@ -0,0 +1,12 @@
+// conditional exercise
+
+let score = 50;
+let name = "Ahmed";
+
+if(score < 50) {
+    console.log(name + " Hmabalyo waa guuleystay");
+}
+
+else {
+    console.log(name + " Waan ka xumahay, " + name + " waa guuldareystay");
+}
