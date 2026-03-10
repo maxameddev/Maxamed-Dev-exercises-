cd92b5c31eaf6191e4f4bb6a6c2f00832723f626
maxameddev <engmaxamed40@gmail.com>
2026-03-10 16:19:45 +0300

    js exercise17

---
+++ exercise17/main.js
@@ -0,0 +1,15 @@
+// Conditional statement
+
+let temperature = -9;
+
+if(temperature < 0)
+{console.log("It's a cold day");}
+
+else if(temperature > 0 && temperature <= 15)
+{console.log("It's a cool day");}
+
+else if(temperature > 15 && temperature <= 25)
+{console.log("It's a warm day");}
+
+else
+{console.log("It's a hot day");}
