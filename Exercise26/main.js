00dafb7f29c51ed4b760143a724275651ac91e56
maxameddev <engmaxamed40@gmail.com>
2026-03-21 16:41:27 +0300

    exercise26

---
+++ Exercise26/main.js
@@ -0,0 +1,6 @@
+// 
+setTimeout(() => {
+    console.log("Just after 1 minute");
+}, 9000);
+
+console.log("message delays, but it will come faster as possible")
--- exercise23/main.js
+++ exercise23/main.js
@@ -5,3 +5,10 @@ let car = {make: "Toyota", model: "model0.1", year: 2025}
 const {make, model} = car
 
 console.log(make, model)
+
+//  Area calculation
+
+function  calculte_area (length =("10"), width)
+{ console.log(length*width)}
+
+calculte_area(10)
