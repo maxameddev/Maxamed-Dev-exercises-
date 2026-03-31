33e98510aed693ed58c7d83d48c1773f38174a18
maxameddev <engmaxamed40@gmail.com>
2026-03-31 17:15:22 +0300

    js exercise32 dom elements

---
+++ exercise32/main.js
@@ -0,0 +1,10 @@
+// dom elements
+
+const header = document.querySelector("h1");
+console.log(header);
+
+const section = document.querySelector("section");
+console.log(section);
+
+const allParagraphs = document.querySelectorAll("p");
+console.log(allParagraphs);
